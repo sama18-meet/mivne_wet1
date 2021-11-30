57b43a0b2245a7760d5b792af58651f1a2b0e14b
Sama Mazzawi <sama.mazz93@gmail.com>
2021-11-30 14:38:06 +0200

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,34 @@
+#include "Player.h"
+#include "Group.h"
+#include "Vec2D.h"
+
+Player::Player(int id, int lvl, Group* group) : id(id), lvl(lvl), group(group), rankVec(lvl, id) {}
+
+int Player::getId() const {
+    return this->id;
+}
+
+int Player::getLvl() const {
+    return this->lvl;
+}
+
+Group* Player::getGroup() const {
+    return this->group;
+}
+
+void Player::setGroup(Group* new_group) {
+    this->group = new_group;
+}
+
+void Player::setGroupStatic(Group* new_group, Player p) {
+    p.setGroup(new_group);
+}
+
+void Player::increaseLvl(int inc) {
+    this->lvl += inc;
+    rankVec.updateX(this->lvl);
+}
+
+const Vec2D* Player::getRankVec() const {
+    return &(this->rankVec);
+}
