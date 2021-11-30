ea5d421361388cfe5713a492bb7b7f9d3ad49a14
urikoelk <95130767+urikoelk@users.noreply.github.com>
2021-11-30 14:04:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include "avl.h"
 
+
+void print_int(int p,int i, int d) {
+    std::cout << i << "\n";
+}
+
+void print_integer_tree (AVL<int, double> tree) {
+    tree.apply_inorder(print_int, 0, true);
+}
 int main() {
-    AVL<int, int> PlayersManager;
-    PlayersManager.insert(10, 15);
+    AVL<int, double> PlayersManager;
+    for (int i = 0; i < 7; ++i) {
+        PlayersManager.insert(i, i+10);
+    }
+    for (int i = 2; i < 7; ++i) {
+        PlayersManager.get_parent(i);
+    }
+    PlayersManager.get_parent(0);
+    for (int i = 0; i < 7; ++i) {
+        std::cout << "key: " << i << " height: " << PlayersManager.get_height_by_key(i) << "\n";
+    }
+    PlayersManager.apply_inorder(print_int, 0, true);
     return 0;
 }
+
