a2b006ebeb3e1f2b6acc173a225e4835d5b2ff87
urikoelk <95130767+urikoelk@users.noreply.github.com>
2021-12-02 12:21:20 +0200

    Update library1.cpp

--- koko/library1.cpp
+++ koko/library1.cpp
@@ -1,16 +1,15 @@
 #include"library1.h"
 #include"PlayersManager.h"
 
-/*
 void* Init() {
-    PlayersManager *DS = new PlayersManager();
-    return (void*)DS;
+    try {
+        auto *DS = new PlayersManager();
+        return (void*)DS;
+    }
+    catch (std::bad_alloc&) {
+        return nullptr;
+    }
 }
-StatusType AddGroup(void *DS, int GroupID){
-    return ((PlayersManager*)DS)-> AddGroup (GroupID);
-}
- */
-
 
 StatusType AddGroup(void *DS, int GroupID) {
     if (DS == nullptr || GroupID <= 0) {
@@ -19,15 +18,107 @@ StatusType AddGroup(void *DS, int GroupID) {
     PlayersManager* pm = (PlayersManager*)DS;
     try {
         bool success = pm->addGroup(GroupID);
+        return success ? SUCCESS : FAILURE;
     }
-    catch (std::bad_alloc) {
+    catch (std::bad_alloc&) {
         return ALLOCATION_ERROR;
     }
-    return success ? SUCCESS : FAILURE;
 }
 
-
-void Quit(void** DS) {
+StatusType AddPlayer(void *DS, int PlayerID, int GroupID, int Level) {
+    if (DS == nullptr || PlayerID <= 0 || GroupID <= 0 || Level < 0) {
+        return INVALID_INPUT;
+    }
     PlayersManager* pm = (PlayersManager*)DS;
-    delete pm;
+    try {
+        bool success = pm->addPlayer(PlayerID, GroupID, Level);
+        return success ? SUCCESS : FAILURE;
+    }
+    catch (std::bad_alloc&) {
+        return ALLOCATION_ERROR;
+    }
+}
+
+StatusType RemovePlayer(void *DS, int PlayerID) {
+    if (DS == nullptr || PlayerID <= 0) {
+        return INVALID_INPUT;
+    }
+    PlayersManager* pm = (PlayersManager*)DS;
+    try {
+        bool success = pm->removePlayer(PlayerID);
+        return success ? SUCCESS : FAILURE;
+    }
+    catch (std::bad_alloc&) {
+        return ALLOCATION_ERROR;
+    }
+}
+
+StatusType ReplaceGroup(void *DS, int GroupID, int ReplacementID) {
+    if (DS == nullptr || GroupID <= 0 || ReplacementID <= 0 || GroupID == ReplacementID) {
+        return INVALID_INPUT;
+    }
+    PlayersManager* pm = (PlayersManager*)DS;
+    try {
+        bool success = pm->replaceGroup(GroupID, ReplacementID);
+        return success ? SUCCESS : FAILURE;
+    }
+    catch (std::bad_alloc&) {
+        return ALLOCATION_ERROR;
+    }
+}
+
+StatusType IncreaseLevel(void *DS, int PlayerID, int LevelIncrease) {
+    if (DS == nullptr || PlayerID <= 0 || LevelIncrease <= 0) {
+        return INVALID_INPUT;
+    }
+    PlayersManager* pm = (PlayersManager*)DS;
+    try {
+        bool success = pm->increaseLevel(PlayerID, LevelIncrease);
+        return success ? SUCCESS : FAILURE;
+    }
+    catch (std::bad_alloc&) {
+        return ALLOCATION_ERROR;
+    }
+}
+
+StatusType GetHighestLevel(void *DS, int GroupID, int *PlayerID) {
+    if (DS == nullptr || PlayerID == nullptr || GroupID == 0) {
+        return INVALID_INPUT;
+    }
+    PlayersManager* pm = (PlayersManager*)DS;
+    try {
+        bool success = pm->getHighestLvl(GroupID, PlayerID);
+        return success ? SUCCESS : FAILURE;
+    }
+    catch (std::bad_alloc&) {
+        return ALLOCATION_ERROR;
+    }
+}
+
+StatusType GetAllPlayersByLevel(void *DS, int GroupID, int **Players, int *numOfPlayers) {
+    if (DS == nullptr || Players == nullptr || numOfPlayers == nullptr || GroupID == 0) {
+        return INVALID_INPUT;
+    }
+    PlayersManager* pm = (PlayersManager*)DS;
+    try {
+        bool success = pm->getAllPlayersByLvl(GroupID, Players, numOfPlayers);
+        return success ? SUCCESS : FAILURE;
+    }
+    catch (std::bad_alloc&) {
+        return ALLOCATION_ERROR;
+    }
+}
+
+StatusType GetGroupsHighestLevel(void *DS, int numOfGroups, int **Players) {
+    if (DS == nullptr || Players == nullptr || numOfGroups < 1) {
+        return INVALID_INPUT;
+    }
+    PlayersManager* pm = (PlayersManager*)DS;
+    try {
+        bool success = pm->getGroupsHighestLvl(numOfGroups, Players);
+        return success ? SUCCESS : FAILURE;
+    }
+    catch (std::bad_alloc&) {
+        return ALLOCATION_ERROR;
+    }
 }
