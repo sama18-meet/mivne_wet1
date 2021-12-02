fcf5dbbd03d13de354025a19b1ec4b36e1946d3c
Sama Mazzawi <sama.mazz93@gmail.com>
2021-12-02 11:07:57 +0200

    Add files via upload

---
+++ koko/library1.cpp
@@ -0,0 +1,27 @@
+#include"library1.h"
+#include"PlayersManager.h"
+
+/*
+void* Init() {
+    PlayersManager *DS = new PlayersManager();
+    return (void*)DS;
+}
+StatusType AddGroup(void *DS, int GroupID){
+    return ((PlayersManager*)DS)-> AddGroup (GroupID);
+}
+ */
+
+
+StatusType AddGroup(void *DS, int GroupID) {
+    if (DS == nullptr || GroupID <= 0) {
+        return INVALID_INPUT;
+    }
+    PlayersManager* pm = (PlayersManager*)DS;
+    try {
+        bool success = pm->addGroup(GroupID);
+    }
+    catch (std::bad_alloc) {
+        return ALLOCATION_ERROR;
+    }
+    return success ? SUCCESS : FAILURE;
+}
