bfda0c2d249fe921bad4ccf301b1a1fc137465a3
urikoelk <95130767+urikoelk@users.noreply.github.com>
2021-12-02 21:25:37 +0200

    Update vec2d.cpp

--- vec2d.cpp
+++ vec2d.cpp
@@ -1,25 +1,26 @@
 #include "Vec2D.h"
-
+#include <iostream>
+using namespace std;
 
 Vec2D::Vec2D(int x, int y) : x(x), y(y) {}
 
-bool Vec2D::operator==(Vec2D v) {
+bool Vec2D::operator==(const Vec2D& v) const {
     return this->x == v.x && this->y == v.y;
 }
 
-bool Vec2D::operator>(Vec2D v) {
-    return this->x > v.x || (this->x == v.x && this->y > v.y);
+bool Vec2D::operator>(const Vec2D& v) const {
+    return this->x > v.x || (this->x == v.x && this->y < v.y);
 }
 
-bool Vec2D::operator>=(Vec2D v) {
+bool Vec2D::operator>=(const Vec2D& v) const {
     return *this == v || *this > v;
 }
 
-bool Vec2D::operator<(Vec2D v) {
+bool Vec2D::operator<(const Vec2D& v) const {
     return !(*this>=v);
 }
 
-bool Vec2D::operator<=(Vec2D v) {
+bool Vec2D::operator<=(const Vec2D& v) const {
     return !(*this>v);
 }
 
@@ -30,3 +31,6 @@ void Vec2D::updateY(int new_y) {
     this->y = new_y;
 }
 
+void Vec2D::print() const {
+    std::cout << "(" << this->x << ", " << this->y << ")\n";
+}
